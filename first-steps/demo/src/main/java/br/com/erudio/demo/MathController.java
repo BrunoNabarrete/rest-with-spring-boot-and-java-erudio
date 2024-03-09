c5afc0df0611c89537d0dba79ae095f0f7ae627e
brunoNabarrete <brunonabarrete@hotmail.com>
2024-03-08 22:05:31 -0300

    finalizando aula 58

--- first-steps/demo/src/main/java/br/com/erudio/demo/MathController.java
+++ first-steps/demo/src/main/java/br/com/erudio/demo/MathController.java
@@ -5,12 +5,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.apache.tomcat.util.http.parser.HttpParser.isNumeric;
+
 @RestController
 public class MathController {
 
     @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
     public Double sum(@PathVariable("numberOne") String numberOne,
-                      @PathVariable("numberTwo") String numberTwo) {
-        return 1d;
+                      @PathVariable("numberTwo") String numberTwo) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception("Teste");
+            }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        return null;
+    }
+
+    private boolean isNumeric(String strNumber) {
+       return false;
     }
 }
